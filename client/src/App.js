33f523b6dcc34618a1507e924300212c6c8b79bf
Shiva <kumarshiva0707@gmail.com>
2024-12-14 00:05:56 +0530

    frontend-deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,17 +17,17 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/create-listing" element={<CreateListing />} />
-          <Route path="/properties/:listingId" element={<ListingDetails />} />
-          <Route path="/properties/category/:category" element={<CategoryPage />} />
-          <Route path="/properties/search/:search" element={<SearchPage />} />
-          <Route path="/:userId/trips" element={<TripList />} />
-          <Route path="/:userId/wishList" element={<WishList />} />
-          <Route path="/:userId/properties" element={<PropertyList />} />
-          <Route path="/:userId/reservations" element={<ReservationList />} />
+          <Route path="https://flatmate-kohl.vercel.app/" element={<HomePage />} />
+          <Route path="https://flatmate-kohl.vercel.app/register" element={<RegisterPage />} />
+          <Route path="https://flatmate-kohl.vercel.app/login" element={<LoginPage />} />
+          <Route path="https://flatmate-kohl.vercel.app/properties/:listingId" element={<ListingDetails />} />
+          <Route path="https://flatmate-kohl.vercel.app/create-listing" element={<CreateListing />} />
+          <Route path="https://flatmate-kohl.vercel.app/properties/category/:category" element={<CategoryPage />} />
+          <Route path="https://flatmate-kohl.vercel.app/properties/search/:search" element={<SearchPage />} />
+          <Route path="https://flatmate-kohl.vercel.app/:userId/trips" element={<TripList />} />
+          <Route path="https://flatmate-kohl.vercel.app/:userId/wishList" element={<WishList />} />
+          <Route path="https://flatmate-kohl.vercel.app/:userId/properties" element={<PropertyList />} />
+          <Route path="https://flatmate-kohl.vercel.app/:userId/reservations" element={<ReservationList />} />
         </Routes>
       </BrowserRouter>
     </div>
